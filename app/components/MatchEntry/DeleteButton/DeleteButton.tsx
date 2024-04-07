69c7b1954cf74b61580e301b4408adf56335c1e8
Pedro Favoreto Gaya <pedrogayaf@gmail.com>
2024-04-07 14:24:38 -0300

    Fix client server component separation

--- app/components/MatchEntry/DeleteButton/DeleteButton.tsx
+++ app/components/MatchEntry/DeleteButton/DeleteButton.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { deleteMatch } from '@/app/lib/actions';
 import styles from './DeleteButton.module.css';
 
--- app/components/MatchEntry/MatchEntry.tsx
+++ app/components/MatchEntry/MatchEntry.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React from 'react'
 import { IMatch } from '../../lib/definitions'
 import clsx from 'clsx'
