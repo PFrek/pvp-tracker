c5a4415d6666e90ddd43c1a6c54eda2555f5fa05
Pedro Favoreto Gaya <pedrogayaf@gmail.com>
2024-04-07 16:37:23 -0300

    Add confirmation to delete button

--- app/components/MatchEntry/DeleteButton/DeleteButton.tsx
+++ app/components/MatchEntry/DeleteButton/DeleteButton.tsx
@@ -1,11 +1,25 @@
 'use client';
 import { deleteMatch } from '@/app/lib/actions';
 import styles from './DeleteButton.module.css';
+import { useState } from 'react';
 
 const DeleteButton = ({ matchId }: { matchId: number }) => {
+  const [confirming, setConfirming] = useState(false);
+
+  const handleConfirmClick = () => {
+    deleteMatch(matchId);
+    setConfirming(false);
+  }
 
   return (
-    <button className={styles.deleteButton} onClick={() => deleteMatch(matchId)}>DEL</button>
+    <div className={styles.container}>
+      {confirming && <div className={styles.confirmationDiv}>
+        <p> Are you sure you want to delete this match?</p>
+        <button className={styles.deleteButton} onClick={handleConfirmClick}>Yes</button>
+        {/* <button className={styles.cancelButton} onClick={() => setConfirming(false)}>No</button> */}
+      </div>}
+      <button className={styles.deleteButton} onClick={() => setConfirming(!confirming)}>DEL</button>
+    </div>
   )
 }
 
