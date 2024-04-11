0ee7742b5c79bc719b6a269dcb1675f49a49f6a2
Pedro Favoreto Gaya <pedrogayaf@gmail.com>
2024-04-10 23:27:43 -0300

    Change Delete button to 'No' during confirmation

--- app/components/MatchEntry/DeleteButton/DeleteButton.tsx
+++ app/components/MatchEntry/DeleteButton/DeleteButton.tsx
@@ -1,7 +1,7 @@
-'use client';
-import { deleteMatch } from '@/app/lib/actions';
-import styles from './DeleteButton.module.css';
-import { useState } from 'react';
+"use client";
+import { deleteMatch } from "@/app/lib/actions";
+import styles from "./DeleteButton.module.css";
+import { useState } from "react";
 
 const DeleteButton = ({ matchId }: { matchId: number }) => {
   const [confirming, setConfirming] = useState(false);
@@ -9,18 +9,28 @@ const DeleteButton = ({ matchId }: { matchId: number }) => {
   const handleConfirmClick = () => {
     deleteMatch(matchId);
     setConfirming(false);
-  }
+  };
 
   return (
     <div className={styles.container}>
-      {confirming && <div className={styles.confirmationDiv}>
-        <p> Are you sure you want to delete this match?</p>
-        <button className={styles.deleteButton} onClick={handleConfirmClick}>Yes</button>
-        {/* <button className={styles.cancelButton} onClick={() => setConfirming(false)}>No</button> */}
-      </div>}
-      <button className={styles.deleteButton} onClick={() => setConfirming(!confirming)}>DEL</button>
+      {confirming && (
+        <div className={styles.confirmationDiv}>
+          <p> Are you sure you want to delete this match?</p>
+          <button className={styles.deleteButton} onClick={handleConfirmClick}>
+            Yes
+          </button>
+          {/* <button className={styles.cancelButton} onClick={() => setConfirming(false)}>No</button> */}
+        </div>
+      )}
+      <button
+        className={styles.deleteButton}
+        onClick={() => setConfirming(!confirming)}
+      >
+        {confirming ? "No" : "DEL"}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default DeleteButton;
+
